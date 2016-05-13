0fdbce650bb15839403cf2ebdfa5409ae6f0557c
tech-camp <tech-camp@p4448110-ipngn24001marunouchi.tokyo.ocn.ne.jp>
2016-05-13 19:38:56 +0900

    add no tagg

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,14 @@ mount_uploader :image, ImageUploader
   # acts_as_taggable_on :labels # post.label_list が追加される
   # acts_as_taggable            # acts_as_taggable_on :tags のエイリアス
 
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:twitter]
+
+  validates :username, presence: true, uniqueness: true
+
+  
+
 end
